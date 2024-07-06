156e1b9dca2880059f8a8b922e7f36fe184cab2d
Pigeon <133509962+Pigeonszz@users.noreply.github.com>
2024-07-06 17:32:10 +0800

    Delete functions directory

--- functions/turnstile-keys.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const keys = {
-    siteKey: context.env.TURNSTILE_SITE_KEY,
-    secretKey: context.env.TURNSTILE_SECRET_KEY,
-  };
-  return new Response(JSON.stringify(keys), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
