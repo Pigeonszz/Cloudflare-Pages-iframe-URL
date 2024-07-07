7b8810a3c0694eb909180ca883007cc4b1efd79e
Pigeon <133509962+Pigeonszz@users.noreply.github.com>
2024-07-07 10:05:57 +0800

    Delete functions directory

--- functions/getSiteTitle.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  return new Response(context.env.SITE_TITLE, {
-    headers: { 'Content-Type': 'text/plain' }
-  });
-}
--- functions/iframe-url.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  return new Response(context.env.IFRAME_URL, {
-    headers: { 'Content-Type': 'text/plain' }
-  });
-}
