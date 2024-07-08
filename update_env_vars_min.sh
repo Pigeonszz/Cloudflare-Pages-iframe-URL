acadf9cdbae17f74e915d127a881971fda6dc332
Pigeon <133509962+Pigeonszz@users.noreply.github.com>
2024-07-08 17:47:53 +0800

    Create update_env_vars_min.sh

---
+++ update_env_vars_min.sh
@@ -0,0 +1,23 @@
+TOKEN=""
+ACCOUNT_ID=""
+curl -X PATCH \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -d '{
+  "deployment_configs": {
+    "production": {
+      "env_vars": {
+        "IFRAME_URL": {
+          "type": "plain_text",
+          "value": "https://iframe_url.com"
+        }
+      }
+    }
+  }
+}' \
+  "https://api.cloudflare.com/client/v4/accounts/$ACCOUNT_ID/pages/projects/$PROJECT_ID"
+sleep 5
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  "https://api.cloudflare.com/client/v4/accounts/$ACCOUNT_ID/pages/projects/$PROJECT_ID/deployments"
