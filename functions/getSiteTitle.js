9616f811a84e85d340c2fad7119bff8db8fd22bd
Pigeon <133509962+Pigeonszz@users.noreply.github.com>
2024-07-09 08:33:27 +0800

    Delete functions/getSiteTitle.js

--- functions/getSiteTitle.js
+++
@@ -1,7 +0,0 @@
-// onRequest 函数处理来自客户端的请求
-export async function onRequest(context) {
-  // 返回包含 SITE_TITLE 环境变量值的纯文本响应
-  return new Response(context.env.SITE_TITLE, {
-    headers: { 'Content-Type': 'text/plain' }
-  });
-}
