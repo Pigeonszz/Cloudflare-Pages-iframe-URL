09be8eed6904b6ba74f7c38e6c7d1ace05804a88
Pigeon <133509962+Pigeonszz@users.noreply.github.com>
2024-07-10 22:57:10 +0800

    Update favicon.js

--- functions/favicon.js
+++ functions/favicon.js
@@ -1,9 +1,9 @@
 // /functions/favicon.js 文件
 
 export async function onRequest(context) {
-  // 获取环境变量 IFRAME_URL 和 SITE_FAVICON
+  // 获取环境变量 IFRAME_URL 和 FAVICON_URL
   const IFRAME_URL = context.env.IFRAME_URL;
-  const SITE_FAVICON = context.env.SITE_FAVICON;
+  const FAVICON_URL = context.env.FAVICON_URL;
 
   // 获取人机验证开关状态
   const turnstileEnabled = true; // 在 Cloudflare Pages 环境中已设置为 true
@@ -20,23 +20,23 @@ export async function onRequest(context) {
     });
   }
 
-  // 如果环境变量 IFRAME_URL 和 SITE_FAVICON 存在
-  if (IFRAME_URL && SITE_FAVICON) {
+  // 如果环境变量 IFRAME_URL 和 FAVICON_URL 存在
+  if (IFRAME_URL && FAVICON_URL) {
     // 将 IFRAME_URL 环境变量按照指定格式分割成数组
     const urls = IFRAME_URL.split(',').map(item => {
       const [url, service] = item.split(';'); // 按分号分割成 URL 和服务名称
       return { url, service }; // 返回对象包含 URL 和服务名称
     });
 
-    // 将 SITE_FAVICON 环境变量按照指定格式分割成数组
-    const siteFavicons = SITE_FAVICON.split(',').map(item => {
+    // 将 FAVICON_URL 环境变量按照指定格式分割成数组
+    const favicons = FAVICON_URL.split(',').map(item => {
       const [service, faviconUrl] = item.split(';'); // 按分号分割成服务名称和 favicon URL
       return { service, faviconUrl }; // 返回对象包含服务名称和 favicon URL
     });
 
     // 构建返回的 favicon 数组
     const faviconUrls = urls.map(urlObj => {
-      const faviconObj = siteFavicons.find(fav => fav.service === urlObj.service); // 查找匹配的 favicon 对象
+      const faviconObj = favicons.find(fav => fav.service === urlObj.service); // 查找匹配的 favicon 对象
       return {
         service: urlObj.service,
         favicon: faviconObj ? faviconObj.faviconUrl : '/favicon.svg' // 如果没有匹配的 favicon 对象，则使用默认的 /favicon.svg
@@ -49,7 +49,7 @@ export async function onRequest(context) {
     });
   } else {
     // 如果环境变量不存在，则返回空响应或错误信息
-    return new Response(JSON.stringify({ error: 'Environment variables IFRAME_URL or SITE_FAVICON not found.' }), {
+    return new Response(JSON.stringify({ error: 'Environment variables IFRAME_URL or FAVICON_URL not found.' }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' }
     });
