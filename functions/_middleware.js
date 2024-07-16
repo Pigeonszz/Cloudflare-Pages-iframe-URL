b5f66f2431d9f7d53eabcbe7c6749211eec64fd2
Pigeon <pigeonsz@proton.me>
2024-07-16 14:28:03 +0800

    Update

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,43 +1,9 @@
 export async function onRequest(context) {
-    // 定义一个函数来生成早期提示
-    function generateEarlyHints(routePath) {
-        switch (routePath) {
-            case '/custom':
-                return [
-                    ['link', '</custom-styles.css>; rel=preload; as=style'],
-                    ['link', '</custom-script.js>; rel=preload; as=script']
-                ];
-            case '/favicon':
-                return [
-                    ['link', '</favicon.ico>; rel=preload; as=image']
-                ];
-            case '/iframe-url':
-                return [
-                    ['link', '</iframe-styles.css>; rel=preload; as=style'],
-                    ['link', '</iframe-script.js>; rel=preload; as=script']
-                ];
-            case '/IP':
-                return []; // 没有特定的资源需要预加载
-            case '/Turnstile':
-                return [
-                    ['link', '</turnstile-styles.css>; rel=preload; as=style'],
-                    ['link', '</turnstile-script.js>; rel=preload; as=script']
-                ];
-            case '/verify-turnstile':
-                return [
-                    ['link', '</verify-turnstile-styles.css>; rel=preload; as=style'],
-                    ['link', '</verify-turnstile-script.js>; rel=preload; as=script']
-                ];
-            default:
-                return [];
-        }
-    }
-
-    // 获取当前请求的路径
-    const routePath = new URL(context.request.url).pathname;
-
-    // 生成早期提示
-    const earlyHints = generateEarlyHints(routePath);
+    // 定义一个包含早期提示的数组，这些提示告诉浏览器预加载资源
+    const earlyHints = [
+        ['link', '</styles.css>; rel=preload; as=style'],  // 预加载样式表
+        ['link', '</script.js>; rel=preload; as=script']   // 预加载脚本文件
+    ];
 
     // 等待下一个中间件或请求处理程序处理请求
     const response = await context.next();
@@ -46,9 +12,7 @@ export async function onRequest(context) {
     const earlyHintResponse = new Response(response.body, response);
 
     // 设置 Link 头部，包含所有早期提示资源的信息
-    if (earlyHints.length > 0) {
-        earlyHintResponse.headers.set('Link', earlyHints.map(([key, value]) => `${key}=${value}`).join(', '));
-    }
+    earlyHintResponse.headers.set('Link', earlyHints.map(([key, value]) => `${key}=${value}`).join(', '));
 
     // 返回修改后的响应对象
     return earlyHintResponse;
