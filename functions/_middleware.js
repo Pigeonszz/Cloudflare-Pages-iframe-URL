fadd46b8d02ea050819485396da616c380e9531b
Pigeon <pigeonsz@proton.me>
2024-07-22 15:46:31 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -15,11 +15,17 @@ export async function onRequest(context) {
     const url = new URL(request.url);
     const requestPath = url.pathname;
   
-    // 检查请求路径是否匹配安全入口
-    if (requestPath.startsWith(secureEntry)) {
-      return await context.next();
+    // 检查请求路径是否为根目录
+    if (requestPath === '/') {
+      // 检查请求路径是否匹配安全入口
+      if (requestPath.startsWith(secureEntry)) {
+        return await context.next();
+      } else {
+        // 如果不匹配安全入口，返回 403 Forbidden
+        return new Response('Forbidden', { status: 403 });
+      }
     } else {
-      // 如果不匹配安全入口，返回 404 404 Not found
-      return new Response('404 Not found', { status: 404 });
+      // 如果不是根目录下的请求，直接继续处理请求
+      return await context.next();
     }
   }
